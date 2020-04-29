4b9b43a4ababece48618133c137f6312aed76577
koustavmandal95 <koustavmandal95@gmail.com>
2020-04-29 23:51:49 +0530

    Update Create.sql

--- Create.sql
+++ Create.sql
@@ -18,6 +18,9 @@ create table student (
   major varchar(20) default 'undecided' unique,
   primary key(student_id)
   );
-  
+ // Auto_increment takes care of the student_id.
 insert into student(name,major) values('Jack','Biology');
-insert into student(name,major) values('John','Sociology'); // Auto_increment takes care of the student_id.
+insert into student(name,major) values('John','Sociology'); 
+insert into student(name,major) values('Claire','Mathematics');
+insert into student(name,major) values('Jane','Zoology');
+insert into student(name,major) values('Jennifier','Botany');
