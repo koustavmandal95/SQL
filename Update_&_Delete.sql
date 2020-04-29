0ed0c88606a0b17fecf27943d1a10193cc56b7b1
koustavmandal95 <koustavmandal95@gmail.com>
2020-04-29 23:45:11 +0530

    Create Update_&_Delete.sql

---
+++ Update_&_Delete.sql
@@ -0,0 +1,6 @@
+select * from student;
+update student set major = 'Bio' where major = 'Biology';
+update student set major = 'Comp Sci' where name = 'Mike';
+
+delete from student where student_id =5;
+
